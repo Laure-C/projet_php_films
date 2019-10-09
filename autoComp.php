598715d8553b2e99eb29d616215cab32a321261d
Laure Chatenet <chatenet@info24-02.iut45.univ-orleans.fr>
2019-10-09 11:53:20 +0200

    modif autocomplet

--- autoComp.php
+++ autoComp.php
@@ -1,6 +1,6 @@
 <?php
-
-$pdo = connect();
+require('connect.php');
+$pdo = connect_bd();
 $keyword = '%'.$_POST['genre'].'%';
 $sql = "SELECT nom_genre FROM genre WHERE nom_genre LIKE (:genre) ORDER BY code_genre ASC LIMIT 0, 10";
 $query = $pdo->prepare($sql);
