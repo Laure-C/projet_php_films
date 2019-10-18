45896f5f1b20ea9250c587152600a43ece2925ca
Julie Decaux <decaux@info27-17.iut45.univ-orleans.fr>
2019-10-18 13:35:39 +0200

    site finalisée, git push non effectué le 16 octobre

--- AjouterFilmRep.php
+++ AjouterFilmRep.php
@@ -18,91 +18,12 @@
     <?php
     echo "<div class = 'main'>";
 
-    function connexion(){
-    try{
-        // $connexion = new PDO('mysql:host=servinfo-mariadb;dbname=DBdecaux;charset=utf8','decaux','decaux');
-        $connexion = new PDO('mysql:host=localhost;dbname=DBchatenet;charset=utf8','root','');
-        return $connexion;
-        }
-    catch(PDOException $e){
-        die("Erreur ".$e->getMessage());
-    }
-}
-
-    function listIdFilm(){
-    $connexion = connexion();
-    $sql = "SELECT code_film from films order by code_film desc";
-    $query = $connexion->query($sql);
-    $connexion = NULL;
-    return $query;
-}
+    require 'sql.php';
 
 
 
 
-function trouverIdGenre($nom){
-    $connexion = connexion();
-    $sql = "SELECT code_genre from genres where nom_genre = $nom ";
-    $query = $connexion->query($sql);
-    $connexion = NULL;
-    return $query;
 
-}
-
-function trouverIdIndividu($nom,$prenom){
-    $connexion = connexion();
-    $sql = "SELECT code_indiv from individus where nom = '$nom' and prenom = '$prenom'";
-    $query = $connexion->query($sql);
-    return $query;
-}
-
-function insertValFilm($titre_org,$titre_fr,$realisateur,$image,$couleur,$pays,$duree,$date){
-    $connexion = connexion();
-    $Id= listIdFilm();
-    $id=0;
-    foreach($Id as $i){
-        if($id == 0){
-            $id =$i['code_film']+1;
-        }
-    }
-    $nom = explode(" ",$realisateur)[0];
-    $prenom = explode(" ",$realisateur)[1];
-    $realisateur = trouverIdIndividu($nom,$prenom);
-    foreach($realisateur as $r){
-        $re =$r['code_indiv'] ;
-    }
-    echo $re;
-    $stmt = $connexion->prepare('INSERT INTO films(code_film,titre_orginal,titre_francais,pays,date1,duree,couleur,realisateur,image) VALUES(:Id,:titre_org,:titre_fr,:pays,:date1,:duree,:couleur,:realisateur,:image)');
-    $stmt -> bindParam(':Id',$id);
-    $stmt -> bindParam(':titre_org',$titre_org);
-    $stmt -> bindParam(':titre_fr', $titre_fr);
-    $stmt -> bindParam(':pays',$pays);
-    $stmt -> bindParam(':date1',$date);
-    $stmt -> bindParam(':duree',$duree);
-    $stmt -> bindParam(':couleur', $couleur);
-    $stmt -> bindParam(':realisateur', $re);
-    $stmt -> bindParam(':image',$image);
-    $stmt -> execute();
-    // couleur radiobutton realisateur liste (nom " " prenom)
-    echo "<br>Vous avez ajouté le film : ".$titre_org;
-}
-
-
-function insertValGenreFilm($genre){
-    $connexion = connexion();
-    $Id= listIdFilm();
-    $id=0;
-    foreach($Id as $i){
-        if($id == 0){
-            $id =$i['code_film'];
-        }
-    }
-    $idgenre = trouverIdGenre($genre);
-    $stmt = $connexion-> prepare('INSERT INTO classificaiton(ref_code_film,ref_code_genre) VALUES(:Id,:genre)');
-    $stmt -> bindParam(':Id',$id);
-    $stmt -> bindParam(':genre',$idgenre);
-    $stmt -> execute();
-}
 
     if(isset($_POST['titreOriginal']) && isset($_POST['titreFrancais']) && isset($_POST['Pays']) && isset($_POST['Date']) && isset($_POST['Duree']) && isset($_POST['couleur']) && isset($_POST['real']) && isset($_POST['genre'])){
 
--- AjouterIndivRep.php
+++ AjouterIndivRep.php
@@ -18,46 +18,9 @@
     <?php
     echo "<div class = 'main'>";
 
-    function connexion(){
-    try{
-        // $connexion = new PDO('mysql:host=servinfo-mariadb;dbname=DBdecaux;charset=utf8','decaux','decaux');
-        $connexion = new PDO('mysql:host=localhost;dbname=DBchatenet;charset=utf8','root','');
-        return $connexion;
-        }
-    catch(PDOException $e){
-        die("Erreur ".$e->getMessage());
-    }
-}
+    require 'sql.php';
+    
 
-function listIdIndiv(){
-    $connexion = connexion();
-    $sql = "SELECT code_indiv from individus order by code_indiv desc";
-    $query = $connexion->query($sql);
-    $connexion = NULL;
-    return $query;
-}
-
-function insertValIndiv($nom,$prenom,$nationnalite,$date_naiss,$date_mort){
-    $connexion = connexion();
-    $Id= listIdIndiv();
-    $id=0;
-    foreach($Id as $i){
-        if($id == 0){
-            $id =$i['code_indiv']+1;
-        }
-    }
-
-    $stmt = $connexion->prepare('INSERT INTO individus(code_indiv,nom,prenom,nationalite,date_naiss,date_mort) VALUES(:code_indiv,:nom,:prenom,:nationalite,:date_naiss,:date_mort)');
-    $stmt -> bindParam(':code_indiv',$id);
-    $stmt -> bindParam(':nom',$nom);
-    $stmt -> bindParam(':prenom', $prenom);
-    $stmt -> bindParam(':nationalite',$nationalite);
-    $stmt -> bindParam(':date_naiss',$date_naiss);
-    $stmt -> bindParam(':date_mort',$date_mort);
-    $stmt -> execute();
-    // couleur radiobutton realisateur liste (nom " " prenom)
-    echo "<br>Vous avez ajouté l'individu : ".$nom;
-}
 
 
 
